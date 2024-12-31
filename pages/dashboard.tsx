ff1d82da357a526bee9b71225a3fd6afb7ebd66f
donuthole8 <muro5866@gmail.com>
2024-12-31 12:12:39 +0900

    ダッシュボード画面を作成

---
+++ pages/dashboard.tsx
@@ -0,0 +1,22 @@
+import { Layout } from '@/components/Layout'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+const Dashboard = () => {
+  const router = useRouter()
+  const logout = async () => {
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
+    router.push('/')
+  }
+
+  return (
+    <Layout title="Task Board">
+      <LogoutIcon
+        className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
+        onClick={logout}
+      />
+    </Layout>
+  )
+}
+
+export default Dashboard
