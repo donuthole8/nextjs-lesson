64b81ec3dda30581977b509a632f1fe82e69a76d
donuthole8 <muro5866@gmail.com>
2025-01-20 02:25:18 +0900

    タスク一覧画面を作成

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,19 @@
 import { Layout } from '@/components/Layout'
+import { TaskForm } from '@/components/TaskForm'
+import { TaskList } from '@/components/TaskList'
+import { UserInfo } from '@/components/UserInfo'
 import { LogoutIcon } from '@heroicons/react/solid'
+import { useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import React from 'react'
 
 const Dashboard = () => {
   const router = useRouter()
+  const queryClient = useQueryClient()
   const logout = async () => {
+    queryClient.removeQueries(['tasks'])
+    queryClient.removeQueries(['user'])
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
     router.push('/')
   }
@@ -17,6 +24,9 @@ const Dashboard = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={logout}
       />
+      <UserInfo />
+      <TaskForm />
+      <TaskList />
     </Layout>
   )
 }
